c94809655da74373d6277c551dab1d3819f96eb7
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-01-11 00:18:39 +0100

    Added JS
    Introduction to JavaScript

--- DOM/index.js
+++ DOM/index.js
@@ -1,2 +1,10 @@
-alert("Hello again");
+// alert("Hello again"); //
 document.querySelector("h1").innerHTML = "Good bye."
+
+function changebuttoncolor() {
+  document.querySelector("button").classList.toggle("buttonred");
+}
+
+function changebackgroudcolor() {
+  document.querySelector("body").classList.toggle("changebackground");
+}
