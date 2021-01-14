60413b4cfb839dcb4c4926f40d6a55b09b473d76
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-01-14 22:17:16 +0100

    Dicee & Drum Kit
    Dicee - finished
    
    Drum Kit - started

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,25 +1,36 @@
+var P1 = 0;
+var P2 = 0;
+var Ev = 0;
+
 function rool() {
 
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+  var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
   var image1 = document.querySelectorAll("img")[0];
 
-  image1.setAttribute("src", randomDiceImage);
+  image1.setAttribute("src", randomDiceImage1);
 
   //.textContent("Link: " + randomDiceImage);
 
-  var randomDiceImage = "images/dice" + randomNumber2 + ".png";
-  document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage);
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
   if (randomNumber1 == randomNumber2) {
     document.getElementsByTagName("h1")[0].innerHTML = "Even!";
-  } else if (randomNumber1<randomNumber2) {
-    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 win!";
-  } else {
-    document.getElementsByTagName("h1")[0].innerHTML = "Player 1 win!";
+    Ev++;
   }
+  else if (randomNumber1<randomNumber2) {
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 wins!";
+    P2++;
+  }
+  else {
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 1 wins!";
+    P1++;
+  }
+
+  document.getElementsByTagName("p")[2].innerHTML = P1 + " . . . . . " + Ev + " . . . . . " + P2;
 
   // var tmp = document.querySelector("#kontrola")
   // console.log(tmp);
