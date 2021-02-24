e5e68561f950b61639673033e194f0b890701c92
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-02-24 23:57:33 +0100

    todolist-v2
    using ejs teplates

---
+++ todolist-v2/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set("view engine", "ejs"); //app.set vs app.use ??
+
+app.use(bodyParser.urlencoded({extended: true}));     // necessary for body-parser
+app.use(express.static("public"));                    // necessary to give access to files in that folder
+
+app.get("/", function(req, res) {
+  let today = new Date();
+  let options = {
+    weekday: "long",  // narrow, short or long
+    day: "2-digit",   // numeric, 2-digit
+    month: "long",    // numeric, 2-digit, narrow, short, long
+    year: "numeric"   //numeric, 2-digit
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+// app.post("/work", function(req, res) {
+//   let item = req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// });
+
+
+app.listen(3000, function() {
+  console.log("Server is running on http://localhost:3000/");
+});
