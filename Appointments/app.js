a9750464048bdb54377691738f13dacfb4a64de0
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-04-17 23:55:31 +0200

    Listen port is changed

--- Appointments/app.js
+++ Appointments/app.js
@@ -343,6 +343,6 @@ if (port == null || port == "") {
   port = 3000;
 };
 
-app.listen(3000, function() {
-  console.log("Server is running on http://localhost:3000/");
+app.listen(port, function() {
+  console.log("Server is running");
 });
