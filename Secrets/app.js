85925cdb78775501d76259010bfde4f29a772e86
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-04-25 23:59:26 +0200

    encrypt, md5 and bcrypt
    how to use those things

--- Secrets/app.js
+++ Secrets/app.js
@@ -4,11 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
-console.log(process.env.API_KEY); // just an example how we can get data from .env file
+// console.log(process.env.API_KEY); // just an example how we can get data from .env file
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -16,7 +19,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema ({
   email: String,
@@ -27,7 +30,7 @@ const userSchema = new mongoose.Schema ({
 
 // Following plugin have to be added before we create mongoose model User
 // We are encrypting only field "password". Other fields are not encrypted.
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 // Encryption is done automaticaly by save and decryption by find
 
 const User = new mongoose.model("User", userSchema);
@@ -47,17 +50,32 @@ app.get("/register", function(req,res) {
 });
 
 app.post("/register", function(req,res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      };
+    });
   });
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    };
-  });
+
+  // const newUser = new User({
+  //   email: req.body.username,
+  //   // password: req.body.password
+  //   password: md5(req.body.password)
+  // });
+  // newUser.save(function(err) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     res.render("secrets");
+  //   };
+  // });
 });
 
 app.post("/login", function(req,res) {
@@ -65,15 +83,38 @@ app.post("/login", function(req,res) {
   const password = req.body.password;
   User.findOne({email: username}, function(err, foundUser) {
     if (err) {
-      console.log(err);;
+      console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+              res.render("secrets");
+            } else {
+              res.send("Password is incorrect!")
+            };
+          });
+        } else {
+          res.send("User not found!");
         };
       };
-    };
-  });
+    });
+
+  // const username = req.body.username;
+  // // const password = req.body.password;
+  // const password = md5(req.body.password);
+  // User.findOne({email: username}, function(err, foundUser) {
+  //   if (err) {
+  //     console.log(err);;
+  //   } else {
+  //     if (foundUser) {
+  //       if (foundUser.password === password) {
+  //         res.render("secrets");
+  //       } else {
+  //         res.send("Password is incorrect!")
+  //       };
+  //     };
+  //   };
+  // });
 });
 
 
