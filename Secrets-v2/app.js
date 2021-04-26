bd4f3fa88f7b160d7ac087af3031c39e89a9f778
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-04-27 00:22:49 +0200

    New project about security
    how to log in using cookies

---
+++ Secrets-v2/app.js
@@ -0,0 +1,107 @@
+//jshint esversion:6
+require("dotenv").config();   // have to be put on top. With this command .env will be active and running
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+// Following have to be exactly in that order
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Following code have to go exactly here
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB-v2", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+// new line
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+// new lines
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.get("/", function(req,res) {
+  res.render("home");
+});
+
+app.get("/login", function(req,res) {
+  res.render("login");
+});
+
+app.get("/register", function(req,res) {
+  res.render("register");
+});
+
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  };
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req,res) {
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() { // it runs only when this line is true
+        res.redirect("/secrets");
+      });
+    };
+  });
+});
+
+app.post("/login", function(req,res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err) { // login function come from passportLocalMongoose or passport
+    if (err) {
+      res.redirect("/login");
+    } else {
+      passport.authenticate("local", {failureRedirect: '/login'})(req, res, function() {
+        res.redirect("/secrets");
+      });
+    };
+  });
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
