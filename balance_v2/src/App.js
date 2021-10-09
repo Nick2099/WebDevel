d103a43c5827095f235c360532e0cc2d36ba4a00
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-10-09 23:41:30 +0200

    React practice

--- balance_v2/src/App.js
+++ balance_v2/src/App.js
@@ -1,3 +1,4 @@
+
 import React, {useState, useContext, useEffect} from 'react';
 import NavigationBar from "./NavigationBar";
 import LoginArea from "./LoginArea";
@@ -8,6 +9,25 @@ import {PageContentContext} from "./PageContentContext";
 
 function App() {
   const [page, setPage] = useContext(PageContentContext);
+  const {REACT_APP_BASE_URL, REACT_APP_DB_HOST, REACT_APP_DB_USER, REACT_APP_DB_PASS} = process.env;
+  console.log(REACT_APP_BASE_URL, REACT_APP_DB_HOST, REACT_APP_DB_USER, REACT_APP_DB_PASS);
+
+  var mysql = require('mysql');
+  var con = mysql.createConnection({
+    host: process.env.REACT_APP_DB_HOST,
+    user: process.env.REACT_APP_DB_USER,
+    password: process.env.REACT_APP_DB_PASS,
+    database: "mybalance"
+  });
+  
+  /* con.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + con.threadId);
+  });
+  */
 
   function ShowLoginArea(props) {
     if (props.show==="true") {
