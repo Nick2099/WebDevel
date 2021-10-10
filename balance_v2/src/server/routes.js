dba50fac68462162ca0c8124094fb4407d0d547b
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-10-10 22:22:37 +0200

    React practice

--- balance_v2/src/server/routes.js
+++ balance_v2/src/server/routes.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
-  password : 'prelude',
+  password : 'pass',
   database : 'mybalance'
 });
 
