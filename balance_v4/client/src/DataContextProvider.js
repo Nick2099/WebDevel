568799cb0dae7a1f5ed5b53fd16dbf91720da6da
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-12-10 23:52:26 +0100

    Practice

--- balance_v4/client/src/DataContextProvider.js
+++ balance_v4/client/src/DataContextProvider.js
@@ -4,13 +4,15 @@ export const DataContext = createContext();
 
 export const DataContextProvider = (props) => {
   const [data, setData] = useState({
+    // informations which "areas" will be displayed
     showLogin: false,
     showHome: false,
     showEntry: false,
     showEntryAdd: false,
     showSettings: false,
-    mainUserID: 0,
-    localUserID: [
+    // info about user
+    userID: 0,      // it's a userID
+    localUser: [    // 0 - for main user, >0 - other users/names for same userID
       {
         id: 0,
         name: "",
